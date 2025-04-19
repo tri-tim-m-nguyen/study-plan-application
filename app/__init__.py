de45d638e1d2b1ecee2ce4ec81b98a337ed5c311
kavishani vimalathasan <kavishanivimalathasan@kavishanis-MacBook-Pro.local>
2025-04-20 00:58:31 +0800

    I have included a secret key inorder to fix the internal server error raised when opening the login/Sign Up page, now it opens without issues

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Best study plan creater" # I have included a secret key to help run the login page
 
 from app import routes

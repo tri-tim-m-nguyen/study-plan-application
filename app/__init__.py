cb867c1555e5a57b47955dd73467b9db75fffdc3
Rayan Tarawneh <rayan.tarawneh4c@gmail.com>
2025-05-02 10:12:23 +0800

    the secret key is no longer available in plain code and will now run from the env

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "Best study plan creater" # I have included a secret key to help run the login page
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key') # I have included a secret key to help run the login page
 
 from app import routes

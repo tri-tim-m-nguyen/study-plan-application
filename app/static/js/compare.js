c69b8b5e187b4071deed590bd4254203a005be4e
tri-tim-m-nguyen <tri121.nguyen@gmail.com>
2025-05-12 19:59:51 +0800

    Minor change based on comments

--- app/static/js/compare.js
+++ app/static/js/compare.js
@@ -150,7 +150,7 @@ function setupRequestButtons() {
 function setupTimetableViewButtons() {
     // View other user's timetable
     document.querySelectorAll('.view-timetable').forEach(button => {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
             const username = this.getAttribute('data-username');
             // Check if the button is already active
             const isActive = this.classList.contains('btn-primary');
